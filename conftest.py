8ccdd86de1aab23151702336b80a274d95acfdbf
Dkratok <D.kratok@gmail.com>
2026-01-03 12:37:54 +0100

    conftest.py added

--- conftest.py
+++ conftest.py
@@ -1,2 +1,20 @@
- 
+import pytest
+from utilities.logging import Logger
 
+logger = Logger(__name__).get_logger()
+
+@pytest.fixture(scope="session", autouse=True)
+def global_setup_teardown():
+    logger.info("=== Test session START ===")
+
+    yield
+
+    logger.info("=== Test session END ===\n")
+
+@pytest.fixture(autouse=True)
+def per_test_setup_teardown():
+    logger.info("=== Test START ===")
+
+    yield
+
+    logger.info("=== Test END ===")
