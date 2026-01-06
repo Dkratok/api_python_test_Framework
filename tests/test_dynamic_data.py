8f4b211f7fa35ec475b87bd888a377b3d7f8a635
Dkratok <D.kratok@gmail.com>
2026-01-06 12:59:01 +0100

    Check UUID version test added.

--- tests/test_dynamic_data.py
+++ tests/test_dynamic_data.py
@@ -1,17 +1,16 @@
 import json
+import uuid
 from api_methods.api_client import ApiClient
-from utilities.get_ip import get_external_ip
 from utilities.logging import Logger
 from utilities.yaml_actions import YamlActions
 
 log = Logger(__name__).get_logger()
 
-def test_dynamic_data():
-    log.info("Starting test_dynamic_data")
-    getip_endpoint = YamlActions("test_data/resources.yaml").get_yaml_value_by_key("get_ip")
-    expected_ip = get_external_ip()
-    responce_content = ApiClient().get(getip_endpoint).content.decode('utf-8')
-    ip_from_tested_service = json.loads(responce_content)["origin"]
-    assert expected_ip == ip_from_tested_service, (
-        f"Expected {expected_ip}, got {ip_from_tested_service}"
-    )
+
+def test_check_uuid4():
+    log.info("Starting test_check_uuid4")
+    getuuid_endpoint = YamlActions("test_data/resources.yaml").get_yaml_value_by_key("get_uuid")
+    response_content = ApiClient().get(getuuid_endpoint).content.decode('utf-8')
+    log.info(f"Response content: {response_content}")
+    uuid_from_service = json.loads(response_content)["uuid"]
+    assert uuid.UUID(uuid_from_service).version == 4, "UUID is not a version 4 or not a valid UUID"
